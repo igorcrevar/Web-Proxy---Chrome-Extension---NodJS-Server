add96220d4bb43b79d25d82140b087c3d498675f
Igor Crevar <crewce@gmail.com>
2012-09-07 02:53:10 +0200

    additional work

--- server/server.js
+++ server/server.js
@@ -1,22 +1,48 @@
 var http = require('http');
+var port = 9615;
 
 http.createServer(function (req, res) {
-	var realUrl = req.url;
-	console.log(realUrl + ' requested');
+	console.log('requested full url ' + req.url);
+	var parsedInput = require('url').parse(req.url, true);
+	
+	var realDomain = parsedInput.query['domain'];
+	var realPath = parsedInput.query['path'];
+	
+	if (!realDomain || !realPath) {
+		return;
+	}
+	
+	//strip prefix http or https
+	realDomain = realDomain.replace(/(?:https?:\/\/)/i, '');
+	
+	console.log(realDomain + ' requested with ' + realPath);
+	
 	var options = {
-		host: 'https://facebook.com',
+		host: realDomain,
 		port: 80,
-		path: realUrl,
+		path: realPath,
 		method: 'GET'
 	};
 	
-	var req = http.request(options, function(res) {
-		console.log('STATUS: ' + res.statusCode);
-		console.log('HEADERS: ' + JSON.stringify(res.headers));
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-			console.log('BODY: ' + chunk);
+	http.request(options, function(proxyCallRes) {
+		var statusCode = proxyCallRes.statusCode;
+		var headers = proxyCallRes.headers;
+		
+		console.log('STATUS: ' + statusCode);
+		console.log('HEADERS: ' + JSON.stringify(headers));
+		
+		//res.setEncoding('utf8');
+		res.writeHead(statusCode, headers);
+			
+		proxyCallRes.on('data', function (chunk) {
+			//res.write(chunk);
+			console.log(chunk);
 		});
-	});
+		
+		proxyCallRes.on('end', function() {
+			res.end();
+		});
+	}).end();
+}).listen(port);
 
-}).listen(9615);
+console.log('waiting on ' + port);
