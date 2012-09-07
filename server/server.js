ef617d1d2716d8b283ac9269c9c819c0d5bc57f4
Igor Crevar <crewce@gmail.com>
2012-09-07 16:46:45 +0200

    rearrange, additional changes

--- server/server.js
+++ server/server.js
@@ -1,48 +1,70 @@
 var http = require('http');
-var port = 9615;
+var https = require("https");
+var serverPort = 9615;
 
 http.createServer(function (req, res) {
 	console.log('requested full url ' + req.url);
 	var parsedInput = require('url').parse(req.url, true);
-	
+
 	var realDomain = parsedInput.query['domain'];
 	var realPath = parsedInput.query['path'];
-	
+
 	if (!realDomain || !realPath) {
 		return;
 	}
-	
+
 	//strip prefix http or https
-	realDomain = realDomain.replace(/(?:https?:\/\/)/i, '');
-	
-	console.log(realDomain + ' requested with ' + realPath);
-	
+	var protocolObject = http;
+	var protocolPort = 80;
+	var protocolType = 'http';
+	realDomain = realDomain.replace(/(https?):\/\/(?::(\d+))?/i, function (fullMatch, protocol, port) {
+		console.log('parsed input!');
+		protocolType = protocol.toLowerCase();
+		protocolObject = protocolType === 'http' ? http : https;
+		if (port) {
+			protocolPort = port;
+		}
+		return '';
+	});
+
+	console.log(realDomain + '  type = ' + protocolType + '  port = ' + protocolPort + '  path = ' + realPath);
+
 	var options = {
 		host: realDomain,
-		port: 80,
+		port: protocolPort,
 		path: realPath,
-		method: 'GET'
+		method: 'GET',
+		headers: req.headers
 	};
-	
-	http.request(options, function(proxyCallRes) {
-		var statusCode = proxyCallRes.statusCode;
-		var headers = proxyCallRes.headers;
+
+	var proxyCallRequest = protocolObject.request(options, function(proxyCallResponse) {
+		var output = '';
+		var statusCode = proxyCallResponse.statusCode;
+		var headers = proxyCallResponse.headers;
 		
 		console.log('STATUS: ' + statusCode);
 		console.log('HEADERS: ' + JSON.stringify(headers));
 		
-		//res.setEncoding('utf8');
-		res.writeHead(statusCode, headers);
-			
-		proxyCallRes.on('data', function (chunk) {
-			//res.write(chunk);
-			console.log(chunk);
+		proxyCallResponse.on('data', function (chunk) {
+			output += chunk;
 		});
-		
-		proxyCallRes.on('end', function() {
+				
+		proxyCallResponse.on('end', function() {		
+			//res.setEncoding('utf8');
+			//res.statusCode = statusCode;
+            //res.send(result);			
+			res.writeHead(statusCode, headers);
+			res.write(output);
 			res.end();
 		});
-	}).end();
-}).listen(port);
+	
+	});
+	
+	proxyCallRequest.on('error', function(err) {
+			res.send('error: ' + err.message);
+	});
+	
+	proxyCallRequest.end();
+}).listen(serverPort);
 
-console.log('waiting on ' + port);
+console.log('waiting on ' + serverPort);
